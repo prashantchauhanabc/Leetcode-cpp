66fd1b75f8dae03dc760bf5107524c929ef3165e
Prashant Chauhan <prashantchauhanabc@gmail.com>
2026-09-16 22:06:02 +0530

    Two sum problem leetcode

---
+++ Twosum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& arr, int target) {
+        int n = arr.size();
+        for(int i=0; i<n-1; i++){
+            for(int j=i+1; j<n; j++){
+                if(arr[i] + arr[j] == target){
+                    return {i,j};
+                }
+            }
+        }
+        return {};
+    }
+};
