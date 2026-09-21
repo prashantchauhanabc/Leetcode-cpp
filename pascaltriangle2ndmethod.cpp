18d9618c7ce65671c8a6ee4723e8cfd55b068464
Prashant Chauhan <prashantchauhanabc@gmail.com>
2026-09-21 22:00:13 +0530

    pasacl triangle

--- pascaltriangle2ndmethod.cpp
+++ pascaltriangle2ndmethod.cpp
@@ -1,3 +1,20 @@
+// class Solution {
+// public:
+//     vector<vector<int>> generate(int numRows) {
+//         vector<vector<int>>  ans;
+//         for(int i =1; i<=numRows; i++){
+//             ans.push_back(vector<int>(i,1));
+//         }
+//         for(int i=0; i<ans.size(); i++){
+//             for(int j=1; j<ans[i].size()-1; j++){
+//                 if(j!=0 && j!=i)
+//                 ans[i][j] = ans[i-1][j-1] + ans[i-1][j];
+//             }
+//         }
+//         return ans;
+        
+//     }
+// };             
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
@@ -5,9 +22,8 @@ public:
         for(int i =1; i<=numRows; i++){
             ans.push_back(vector<int>(i,1));
         }
-        for(int i=0; i<ans.size(); i++){
+        for(int i=2; i<ans.size(); i++){
             for(int j=1; j<ans[i].size()-1; j++){
-                if(j!=0 && j!=i)
                 ans[i][j] = ans[i-1][j-1] + ans[i-1][j];
             }
         }
